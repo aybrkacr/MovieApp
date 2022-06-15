ee2435472c66973c3f95f9048b8400a81c0d8045
ayberk.acar <ayberk.acar@globetech.io>
2022-06-15 15:41:41 +0300

    Styles added

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Movie } from "../movie";
 
 @Component({
     selector: 'movies',
@@ -6,5 +7,12 @@ import { Component } from "@angular/core";
     styleUrls: ['movies.component.css']
 })
 export class MoviesComponent {
+
+    title = "Movies List";
+
+    movie: Movie = {
+        id: 1,
+        name: 'Movie Name'
+    }
     
 }
